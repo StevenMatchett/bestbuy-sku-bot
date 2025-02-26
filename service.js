95f286fd9b5e85dc66e1d8067f63a2621a9f351a
Steven Matchett <steve.matchett@move.com>
2025-02-26 11:07:21 -0500

    update

--- service.js
+++ service.js
@@ -1,162 +1,100 @@
 "use strict";
 
-var player = require('play-sound')()
-const open = require('open');
-const fetch = require('node-fetch');
-let url = "https://www.bestbuy.com/api/3.0/priceBlocks?skus="
+var player = require("play-sound")();
+const open = require("open");
+const fetch = require("node-fetch");
 
-const skus = [
+const skus = [6620367, 6614744, 6619458, 6620605, 6620368];
 
-    6429442,
-    6439299,
-    6438278,
-    6439384,
-    6439128,
-    6439385,
-    6437909,
-    6439127,
-    6437912,
-    6439300,
-    6439301,
-    6432653,
-    6432654,
-    "prod25090928", // Sams club
-    "prod25090927",
-];
-
-
-const promiseResolver = async (promise) => {
-    return await new Promise((resolve, reject) => {
-
-        const timer = setTimeout(() => {
-            resolve(null)
-        }, 2000)
-
-        promise.then(value => {
-            clearTimeout(timer)
-            resolve(value)
-        }).catch(reason => {
-            clearTimeout(timer)
-            resolve(null)
-        })
-
-    })
-}
-
-
-const found = (res) => {
-    open("https://bestbuy.com" + res[0]["sku"]['url']);
-    player.play('alarm.mp3', { timeout: 300 }, function(err){
-        if (err) throw err
-    })
-}
+const found = (sku) => {
+  open("https://www.bestbuy.com/site/searchpage.jsp?st=" + sku);
+  player.play("alarm.mp3", { timeout: 300 }, function (err) {
+    console.log("Error playing sound:", err);
+    if (err) throw err;
+  });
+};
 
 let resolved = 0;
 
 const speed = () => {
-    setTimeout(()=>{
-        console.log(`Resolved ${resolved} in 1 min`)
-        resolved = 0;
-        speed();
-    },60000)
-}
+  setTimeout(() => {
+    console.log(`Resolved ${resolved} in 1 min`);
+    resolved = 0;
+    speed();
+  }, 60000);
+};
 speed();
 
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
 const parseBestbuy = async (sku) => {
-    let res = bestbuy(sku)
-    res = await promiseResolver(res);
-    if (res){
-        res = await res.json();
-        if (res[0] && res[0]['sku'] && res[0]["sku"]['buttonState']){
-            if (res[0]["sku"]['buttonState']['buttonState'] !== "SOLD_OUT"){
-                found(res)
-                return 69;
-            }
-        }
-        resolved++;
-    }
-    return 0;
-}
+  let res = await bestbuy(sku);
+  resolved++;
+  if (res) {
+    found(sku);
+    return 69;
+  }
+  await sleep(10000);
+  return 0;
+};
 
-const parseSams = async (sku) => {
-    let res = samsClub(sku)
-    res = await promiseResolver(res);
+const bestbuy = async (sku) => {
+  const res = await fetch(
+    `https://www.bestbuy.com/fulfillment/shipping/api/v1/fulfillment/sku;skuId=${sku};profileCode=10130;postalCode=15717;deliveryDateOption=EARLIEST_AVAILABLE_DATE;selectedDeliveryServiceSkuIds=?paidMembership=false&planPaidMemberType=NULL`,
+    {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        Accept: "application/json",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Cache-Control": "no-cache",
+        Pragma: "no-cache",
+        Priority: "u=1, i",
+        "Sec-Ch-Ua": '"Chromium";v="133", "Not(A:Brand";v="99"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": '"macOS"',
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "X-Client-Id": "FRV",
+        "X-Request-Id": "BROWSE",
+      },
+      referrer:
+        "https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-5070-ti-gaming-oc-16g-gddr7-pci-express-5-0-graphics-card-black/6618884.p?skuId=6618884",
+      referrerPolicy: "strict-origin-when-cross-origin",
+      method: "GET",
+      mode: "cors", // Only needed for client-side requests
+      credentials: "include", // Only needed for client-side requests
+    },
+  );
 
-    try {
-        res = await res.json();
-        let aval = res.payload.products[0].skus[0].onlineOffer.inventory.status;
-        if (aval !== "NOT_AVAILABLE"){
-            open("https://www.samsclub.com/p/asdf/" + sku);
-            player.play('alarm.mp3', { timeout: 300 }, function(err){
-                if (err) throw err
-            })
-            return 69;
-        }
-    } catch (err) {
-        console.warn(err)
-    }
-
-    return 0;
-}
-
-const bestbuy = (sku) => {
-    const headers = {
-        'authority': 'www.bestbuy.com',
-        'pragma': 'no-cache',
-        'cache-control': 'no-cache',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'none',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
-        'accept-language': 'en-US,en;q=0.9',
-        'Cookie': 'UID=b5d1a076-57ab-4ccf-8c0f-d9009d590169; pst2=588|N; oid=488954692; vt=fd76350b-3ce2-11eb-be65-121fe65aa3d5; CTT=67c011be4d769cb28467a8702e9f9c6e; SID=4f6a54e1-c268-4c4c-89fe-6e193ad18357; optimizelyEndUserId=oeu1607823118997r0.46213488756067034; rxVisitor=1607823119040VN3G5R4D1T4DGH4CBEJJ671C4CO46C7J; COM_TEST_FIX=2020-12-13T01%3A31%3A59.961Z; locDestZip=26501; locStoreId=832; sc-location-v2=%7B%22meta%22%3A%7B%22CreatedAt%22%3A%222020-12-13T01%3A32%3A02.645Z%22%2C%22ModifiedAt%22%3A%222020-12-13T01%3A32%3A03.467Z%22%2C%22ExpiresAt%22%3A%222021-12-13T01%3A32%3A03.467Z%22%7D%2C%22value%22%3A%22%7B%5C%22physical%5C%22%3A%7B%5C%22zipCode%5C%22%3A%5C%2226501%5C%22%2C%5C%22source%5C%22%3A%5C%22A%5C%22%2C%5C%22captureTime%5C%22%3A%5C%222020-12-13T01%3A32%3A02.645Z%5C%22%7D%2C%5C%22destination%5C%22%3A%7B%5C%22zipCode%5C%22%3A%5C%2226501%5C%22%7D%2C%5C%22store%5C%22%3A%7B%5C%22storeId%5C%22%3A832%2C%5C%22zipCode%5C%22%3A%5C%2226501%5C%22%2C%5C%22storeHydratedCaptureTime%5C%22%3A%5C%222020-12-13T01%3A32%3A03.466Z%5C%22%7D%7D%22%7D; partner=212%261%262020-12-12+19%3a32%3a03.000; dtCookie=v_4_srv_9_sn_C2I98A5FVDC2F8QNRHUBT1KR3QQHSMGM_app-3A1b02c17e3de73d2a_1_ol_0_perc_100000_mul_1; c6db37d7c8add47f1af93cf219c2c682=628232e5d88b0aa13219638ca8559caa; basketTimestamp=1607899376307; physical_dma=508; customerZipCode=26501|N; bby_rdp=l; dtSa=-; rxvt=1609861331340|1609859457419; dtPC=9$59527927_386h-vHGKKECHHOAFVTRSVPIFRBOAPISVREPCD-0e5; dtLatC=158; _abck=6985DEBF82A60338B3BF46B12BB9EED9~-1~YAAQJA8kF1kqeHJ2AQAAPb1z1AUuWYyv4imsXbLQw1rDI0pTO+Lhm09+lFE6ZwtCAmJ0cwP28EEqw5DRQRUrQXttBy1Lgb+RxlHU+VjpwonKPqerFKfM2qBqrYXz65nghxn7bgo+y6GcUFkfuRZNdc8wf51mWwHhBU3LWsmAvwjq/RODvFex2IzL32XDY89V9+6r8k0fKurBSn8jNghoZ606vprm8isw3ywmvggXVjBtep3IA77pSHbPpG1ji7fdF6HRxzTsf+6/3/K009sXiy9vO+jZcunb+r/PE2nK/Z53Ohd7p2pEAuiqFVjBWMNz2OtX4IYWEHy4jZ393zTYYjToxKyMRpY=~0~-1~-1; bm_sz=BC6B0EE71448F215FF4E0CEF09F68D89~YAAQJA8kF1gqeHJ2AQAAPb1z1ApFjHkYMlgHbUL/A18dQWkDgfCClTElQPcazWNsYtJJcc0PIz2IBavCpTVgeffsB5zMNfjkAE7gOxXjvzK42Sl0I3sEhAKHoAviAbg3CaG/f+t7wqtDzwGxMTXV22yBrQlzbn1fnDz92tyTINPtiEYR8yWn9T4isXBl1NuF7w==; bby_cbc_lb=p-browse-e'
-    };
-    return fetch(url + sku, {
-        headers: {...headers}
-    });
-}
-
-const samsClub = (productId) => {
-    return fetch("https://www.samsclub.com/api/node/vivaldi/v2/az/products", {
-        "headers": {
-            "accept": "application/json, text/plain, */*",
-            "accept-language": "en-US,en;q=0.9",
-            "cache-control": "no-cache",
-            "content-type": "application/json",
-        },
-        "body": "{\"productIds\":[\""+ productId +"\"],\"type\":\"LARGE\",\"clubId\":4936}",
-        "method": "POST",
-        "mode": "cors"
-    });
-}
-
-
-
-
-
+  const data = await res.json();
+  if (!data || !data.responseInfos || !data.responseInfos[0]) {
+    console.error("Error fetching data");
+    return false;
+  }
+  return data.responseInfos[0].shippingEligible;
+};
 const run = async () => {
-    for (let i = 0; i < skus.length; i++ ){
-        let returnd = 0;
+  for (let i = 0; i < skus.length; i++) {
+    let returnd = 0;
 
-        if (Number.isInteger(skus[i])){
-            returnd = await parseBestbuy(skus[i]);
-        } else {
-            returnd = await parseSams(skus[i]);
-        }
-
-        if (returnd === 69) return;
-
+    if (Number.isInteger(skus[i])) {
+      returnd = await parseBestbuy(skus[i]);
+    } else {
+      // returnd = await parseSams(skus[i]);
     }
 
-    return await run();
-}
+    if (returnd === 69) return;
+  }
+
+  return await run();
+};
 
 const main = async () => {
-    run();
-}
+  run();
+};
 
 main();
